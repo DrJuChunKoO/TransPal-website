164547da412c5c484eb5fe83017af5f9347382b1
gnehs <jayuiop@gmail.com>
2024-02-22 14:40:35 +0800

    updated SpeechesList

--- components/SpeechesList.tsx
+++ components/SpeechesList.tsx
@@ -10,10 +10,12 @@ export default function SpeechesList({ Speeches }: { Speeches: any[] }) {
           className="flex gap-2 items-center hover:bg-gray-50 active:bg-gray-100 rounded p-2"
         >
           <DocumentTextIcon className="size-6" />
-          <span className="text-gray-500 tabular-nums tracking-tight">
-            {speech.date}
-          </span>
-          <span>{speech.name}</span>
+          <div className="flex flex-wrap">
+            <span className="text-gray-500 tabular-nums tracking-tight mr-2">
+              {speech.date}
+            </span>
+            <span>{speech.name}</span>
+          </div>
         </Link>
       ))}
     </div>
