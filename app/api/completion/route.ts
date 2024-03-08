0771661c7efb5722b847a2c420ce22001a1ae92c
gnehs <jayuiop@gmail.com>
2024-03-08 14:19:52 +0800

    added vercel ai

---
+++ app/api/completion/route.ts
@@ -0,0 +1,48 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { promises as fs } from "fs";
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+// Set the runtime to edge for best performance
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { messages, filename } = await req.json();
+  try {
+    await fs.access(process.cwd() + "/public/speeches/" + filename + ".json");
+    const speechFile = JSON.parse(
+      await fs.readFile(
+        process.cwd() + "/public/speeches/" + filename + ".json",
+        "utf-8"
+      )
+    );
+    let speechMessages = speechFile.content.map(
+      ({ speaker, text }: { speaker: string; text: string }) => ({
+        speaker,
+        text,
+      })
+    );
+    const systemPrompt = `你是會議逐字稿的 AI 助手，下列是會議逐字稿的一部分。
+${JSON.stringify(speechMessages)}
+請根據上述內容回答使用者的問題，若無法回答請告知使用者「無法回答」。
+請不要回答與上述內容無關的問題。`;
+
+    // Ask OpenAI for a streaming completion given the prompt
+    const response = await openai.chat.completions.create({
+      model: prompt.length > 7500 ? "gpt-3.5-turbo-16k" : "gpt-3.5-turbo",
+      stream: true,
+      temperature: 0.6,
+      max_tokens: 1024,
+      messages: [{ role: "system", content: systemPrompt }, ...messages],
+    });
+    // Convert the response into a friendly text-stream
+    const stream = OpenAIStream(response);
+    // Respond with the stream
+    return new StreamingTextResponse(stream);
+  } catch (e) {
+    return new Response("File not found", { status: 404 });
+  }
+}
