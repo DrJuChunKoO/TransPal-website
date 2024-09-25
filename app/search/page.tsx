077e5a480d07769210540d2aa94a4c6b00849a4f
gnehs <gnehs@gnehs.net>
2024-09-25 19:45:35 +0800

    fixed Missing Suspense boundary with useSearchParams

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,11 +1,14 @@
 import Search from "@/components/Search";
+import { Suspense } from "react";
 export default function Page() {
   return (
     <div className="container my-10">
       <div className="text-4xl font-bold text-gray-800 dark:text-white">
         搜尋
       </div>
-      <Search />
+      <Suspense>
+        <Search />
+      </Suspense>
     </div>
   );
 }
